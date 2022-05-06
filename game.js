6c3de57ecc0cf38e1656f1d618dafc464d75ecaf
John Colagioia <john@colagioia.net>
2022-05-06 06:31:40 -0400

    Add game die

--- game.js
+++ game.js
@@ -5,9 +5,28 @@ const pieces = [
 ];
 
 window.addEventListener('load', (e) => {
+  const roll = document.getElementById('roll');
   const start = document.getElementById('sq000');
 
+  roll.addEventListener('click', (e) => {
+    rollDie();
+  });
   start.innerHTML = '<span class="game-piece" id="player-1">' +
     pieces[Math.trunc(Math.random() * pieces.length)] +
     '</span>';
 });
+
+function rollDie() {
+  const tray = document.getElementById('dice-tray');
+  const roll = Math.trunc(Math.random() * 6) + 1;
+  const button = document.getElementById('roll');
+
+  tray.innerHTML = `<b>${faces[roll - 1]}</b>`;
+  p1.draggable = true;
+
+  if (document.getElementById(id)) {
+    button.disabled = true;
+    dropTargets.push(id);
+  }
+}
+
