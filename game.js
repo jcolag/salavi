8b827a8854c838b2a912aad6d554e1ff2f671429
John Colagioia <john@colagioia.net>
2022-05-07 06:50:08 -0400

    Add some loose drag-and-drop code

--- game.js
+++ game.js
@@ -8,6 +8,9 @@ window.addEventListener('load', (e) => {
   const roll = document.getElementById('roll');
   const start = document.getElementById('sq000');
 
+  document.addEventListener('drag', () => {}, false);
+  document.addEventListener('dragover', (e) => e.preventDefault(), false);
+  document.addEventListener('drop', dropPiece);
   roll.addEventListener('click', (e) => {
     rollDie();
   });
@@ -20,9 +23,11 @@ function rollDie() {
   const tray = document.getElementById('dice-tray');
   const roll = Math.trunc(Math.random() * 6) + 1;
   const button = document.getElementById('roll');
+  const p1 = document.getElementById('player-1');
 
   tray.innerHTML = `<b>${faces[roll - 1]}</b>`;
   p1.draggable = true;
+  p1.addEventListener('dragstart', dragPiece);
 
   if (document.getElementById(id)) {
     button.disabled = true;
@@ -30,3 +35,21 @@ function rollDie() {
   }
 }
 
+function dragPiece(event) {
+  dragged = event.target;
+}
+function dropPiece(event) {
+  var target = event.target;
+
+  if (target.className.indexOf('game-piece') >= 0) {
+    target = target.parentElement;
+  }
+
+  event.preventDefault();
+  dragged.parentNode.removeChild(dragged);
+  target.appendChild(dragged);
+  dragged = null;
+}
+function ignore(event) {
+  event.preventDefault();
+}
