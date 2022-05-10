ed5af7e23208366739cd709874ed3aa368342901
John Colagioia <john@colagioia.net>
2022-05-10 06:29:44 -0400

    Mark squares as the start of a "snake" or "ladder"

--- generate.js
+++ generate.js
@@ -9,6 +9,7 @@ const head = '<!DOCTYPE html><html lang="en"><head>' +
 const foot = '</table><br><button id="roll">Roll Die</button>' +
   '<br><div id="dice-tray">&nbsp;</div></body></html>';
 const elements = [];
+const channels = [];
 let size = 4;
 
 if (!isNaN(process.argv[2])) {
@@ -17,6 +18,22 @@ if (!isNaN(process.argv[2])) {
 
 const wd = 50 / size;
 
+for (let chan = 0; chan < size; chan++) {
+  let startY = Math.floor(Math.random() * size);
+
+  channels.push({
+    from: {
+      x: Math.floor(Math.random() * size),
+      y: startY,
+    },
+    to: {
+      x: Math.floor(Math.random() * size),
+      y: Math.floor(Math.random() * size / 2)
+        + (startY > size / 2 ? 0 : (size / 2)),
+    },
+  });
+}
+
 for (let row = 0; row < size; row++) {
   const dir = row % 2 === 1 ? 1 : -1;
   const rowClass = row % 2 === 0 ? 'rightward' : 'leftward';
@@ -24,9 +41,21 @@ for (let row = 0; row < size; row++) {
 
   elements.push('</tr>');
   for (let col = 0; col < size; col++) {
+    let dest = '';
+
+    for (let i = 0; i < channels.length; i++) {
+      const ch = channels[i];
+
+      if (number === ch.from.y * size + ch.from.x) {
+        const to = `000${ch.to.y * size + ch.to.x}`.slice(-3);
+
+        dest = ` jump="sq${to}"`;
+      }
+    }
+
     id = `000${number}`.slice(-3);
     elements.push(`<td class="square" draagable="false" id="sq${id}" ` +
-      `style="height: ${wd}vw; width: ${wd}vw;"></td>`);
+      `${dest}style="height: ${wd}vw; width: ${wd}vw;"></td>`);
     number += dir;
   }
 
